aedac8db46886cfab5419c3a822d875c0b5f86bc
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2025-04-11 11:25:50 -0700

    Created a project

---
+++ untitled/src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int rowCount = 21;
+        int columnCount = 19;
+        int titleSize = 32;
+        int boardWidth = columnCount * titleSize;
+        int boardHeight = rowCount * titleSize;
+
+        JFrame frame = new JFrame("PacMan");
+//       frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacnamGame = new PacMan();
+        frame.add(pacnamGame);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
